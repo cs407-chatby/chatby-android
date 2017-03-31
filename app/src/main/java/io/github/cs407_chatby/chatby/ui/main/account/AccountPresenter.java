80e8ed6dcb657db5a1da6dcb14135f0e8887ad3a
Sargun Vohra <sargun.vohra@gmail.com>
2017-03-30 21:50:11 -0400

    merged

--- app/src/main/java/io/github/cs407_chatby/chatby/ui/main/account/AccountPresenter.java
+++ app/src/main/java/io/github/cs407_chatby/chatby/ui/main/account/AccountPresenter.java
@@ -3,11 +3,20 @@ package io.github.cs407_chatby.chatby.ui.main.account;
 
 import javax.inject.Inject;
 
+import io.github.cs407_chatby.chatby.data.model.PatchPassword;
+import io.github.cs407_chatby.chatby.data.service.ChatByService;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+
 public class AccountPresenter implements AccountContract.Presenter {
-    AccountContract.View view;
+
+    private AccountContract.View view;
+
+    private final ChatByService service;
 
     @Inject
-    public AccountPresenter() {}
+    public AccountPresenter(ChatByService service) {
+        this.service = service;
+    }
 
     @Override
     public void onAttach(AccountContract.View view) {
@@ -21,11 +30,21 @@ public class AccountPresenter implements AccountContract.Presenter {
 
     @Override
     public void onSavePressed(String email, String username, String firstName, String lastName) {
-        // TODO
+        service.getCurrentUser()
+                .flatMap(user -> service.putUser(user.getId(), user
+                        .withEmail(email)
+                        .withUsername(username)
+                        .withFirstName(firstName)
+                        .withLastName(lastName)))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(user -> view.showSuccess(), error -> view.showError("Failed to update user!"));
     }
 
     @Override
     public void onUpdatePassPressed(String password) {
-        // TODO
+        service.getCurrentUser()
+                .flatMap(user -> service.patchUserPassword(user.getId(), new PatchPassword(password)))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(user -> view.showSuccess(), error -> view.showError("Failed to update password!"));
     }
 }
